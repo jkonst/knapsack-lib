7aaf46cb7c23030381d218118f9e47376a2e5ab4
root <yannis.konstas@gmail.com>
2019-04-14 15:05:00 +0300

    Adding implementation of ItemCalculator

--- src/main/java/com/mobiquityinc/utils/KnapsackCalculator.java
+++ src/main/java/com/mobiquityinc/utils/KnapsackCalculator.java
@@ -1,4 +1,21 @@
 package com.mobiquityinc.utils;
 
-public class KnapsackCalculator {
+public class KnapsackCalculator implements ItemCalculator {
+
+    public KnapsackCalculator(Double maxWeight, Double[] weights, Double[] values) {
+        this.maxWeight = maxWeight;
+        this.weights = weights;
+        this.values = values;
+    }
+
+    private Double maxWeight;
+    private Double[] weights;
+    private Double[] values;
+
+    @Override
+    public String getItems() {
+        //TODO
+        return null;
+    }
+
 }
