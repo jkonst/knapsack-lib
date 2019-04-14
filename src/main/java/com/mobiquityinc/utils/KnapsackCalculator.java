84b9e82e1561aa523c258cce77a1e6d62c7cfbeb
root <yannis.konstas@gmail.com>
2019-04-14 16:20:59 +0300

    removing constructor with arguments + adding the appropriate getters/setters

--- src/main/java/com/mobiquityinc/utils/KnapsackCalculator.java
+++ src/main/java/com/mobiquityinc/utils/KnapsackCalculator.java
@@ -2,12 +2,6 @@ package com.mobiquityinc.utils;
 
 public class KnapsackCalculator implements ItemCalculator {
 
-    public KnapsackCalculator(Double maxWeight, Double[] weights, Double[] values) {
-        this.maxWeight = maxWeight;
-        this.weights = weights;
-        this.values = values;
-    }
-
     private Double maxWeight;
     private Double[] weights;
     private Double[] values;
@@ -18,4 +12,28 @@ public class KnapsackCalculator implements ItemCalculator {
         return null;
     }
 
+
+    public Double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(Double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public Double[] getWeights() {
+        return weights;
+    }
+
+    public void setWeights(Double[] weights) {
+        this.weights = weights;
+    }
+
+    public Double[] getValues() {
+        return values;
+    }
+
+    public void setValues(Double[] values) {
+        this.values = values;
+    }
 }
