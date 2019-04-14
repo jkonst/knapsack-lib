8d93f468edbd63adc7074309fdf89490d561e1d8
root <yannis.konstas@gmail.com>
2019-04-14 15:06:27 +0300

    Initial description of steps in Packer static pack method

--- src/main/java/com/mobiquityinc/packer/Packer.java
+++ src/main/java/com/mobiquityinc/packer/Packer.java
@@ -1,4 +1,19 @@
 package com.mobiquityinc.packer;
 
+import com.mobiquityinc.exception.APIException;
+
 public class Packer {
+
+public static String pack(String filePath) throws APIException {
+    // 1. Instantiate KnapsackParser
+    // 2. Call parse()
+    // 3. Get lists of weights-values from KnapsackParser
+    // 4. Iterate through max weights
+    // 5. Instantiate a KnapsackCalculator object for each max weight
+    // 6. Call getItems()
+    // 7. Build String response
+    // 8. End of iteration - Return response
+    return null;
+}
+
 }
